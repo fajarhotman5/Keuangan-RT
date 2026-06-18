f9bb173adb66563c6064e094537183cd7e109ddd
fajarhotman5 <fajarhotman5@gmail.com>
2026-06-18 09:37:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -264,6 +264,7 @@ elif st.session_state.menu_aktif == 'unduh':
             st.warning("Data kosong untuk rentang tanggal tersebut.")
         else:
             # Waktu unduh real-time sesuai device saat ini
+            waktu_wib = datetime.now() + timedelta(hours=7)
             waktu_cetak = datetime.now().strftime("%d-%m-%Y %H:%M:%Y"[:16])
             
             col_b1, col_b2 = st.columns(2)
